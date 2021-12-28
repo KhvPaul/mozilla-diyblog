9ec688d03e9b3b31ace996c4b6e5d31f6da6ec14
Paul Khv <paul.khvaliuk@icloud.com>
2021-12-29 01:12:40 +0200

    Update ALLOWED_HOSTS with site and development server URL

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-=jhm2g(4t$3zq4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['', '127.0.0.1']
+ALLOWED_HOSTS = ['frozen-wildwood-71522.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
